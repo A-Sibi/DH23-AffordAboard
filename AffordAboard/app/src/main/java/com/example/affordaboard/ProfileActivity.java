5ec9b4db9243921231dd7fbd77c8d32174f3cac6
A-Sibi <aleksa.sibin@gmail.com>
2023-05-13 20:58:15 +0200

    added Myprofile basic design

---
+++ AffordAboard/app/src/main/java/com/example/affordaboard/ProfileActivity.java
@@ -0,0 +1,19 @@
+package com.example.affordaboard;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import java.util.ArrayList;
+
+public class ProfileActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+
+}
